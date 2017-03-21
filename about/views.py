419e6d4660fb481e4dc9f03767b4a3bd8c1d0f07
michoy <michael.hoyer3@gmail.com>
2017-03-21 23:35:30 +0100

    login start

--- about/views.py
+++ about/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import RegisterForm, LoginForm
 
 
-#Spartansk placeholder
 def login_view(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         if 'login-submit' in request.POST:
             form = LoginForm(request.POST or None)
             if form.is_valid():
@@ -13,14 +13,14 @@ def login_view(request):
                 if user:
                     login(request, user)
                     return HttpResponseRedirect("/overview/")  # Redirect to a success page.
-            return render(request, 'index.html', {'form': form })        # Redirect etter feilet innlogging
+            return render(request, 'about.html', {'form': form})        # Redirect etter feilet innlogging
         elif 'register-submit' in request.POST:
             form = RegisterForm(request.POST or None)
             if form.is_valid():
                 form.register()
-                return render(request, 'index.html')  # Redirect etter registrering
+                return render(request, 'about.html')  # Redirect etter registrering
             else:
-                return render(request, 'index.html', {'form': form })  # Redirect etter feilet registrering
+                return render(request, 'about.html', {'form': form})  # Redirect etter feilet registrering
     else:
-        return render(request, 'index.html')
+        return render(request, 'about.html')
 
--- pineapple/urls.py
+++ pineapple/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 import overview.views
@@ -26,5 +26,6 @@ import botTester.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^base/', exercise.views.base),
+    url('^', include('django.contrib.auth.urls')),
     url(r'^overview/', overview.views.courses),
     ]
