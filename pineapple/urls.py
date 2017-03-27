f533e7d4299fe3b8ddd351f53466331b055217a7
michoy <michael.hoyer3@gmail.com>
2017-03-27 16:29:46 +0200

    clean up

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -27,25 +27,15 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^base/', exercise.views.base),
     #url(r'^login/$', auth_views.login, {'template_name':'registration/login.html'}, name='login'),
-<<<<<<< HEAD
     #url(r'^login/$', about.views.login_view, name='/login/'),
     url(r'^login/$', about.views.do_login),
     url(r'^logout/$', about.views.do_logout),
     url(r'^register/$', about.views.register),
-    url(r'^course/$', course.views.lecturer_course_view, name='/course/'),
     url(r'^about/', about.views.about),
-    url(r'^exercise/(?P<exer_id>[0-9]+)/$', exercise.views.do_exercise),
-    url(r'^course/(?P<fagkode>[\w]{7})/$', course.views.delegate_course_view), # TODO VI KAN KUN HA 7-TEGN FAGKODER!
-    url(r'^about/$', about.views.about),
-    url(r'^exercise/$', exercise.views.do_exercise),
-    url(r'^lecturer_course/$', course.views.lecturer_course),
     url(r'^overview/$', overview.views.courses),
-=======
-    url(r'^login/$', about.views.login_view, name='/login/'),
-    url(r'^about/', about.views.about),
-    url(r'^exercise/(?P<exer_id>[0-9]+)/$', exercise.views.do_exercise),
+    url(r'^course/$', course.views.lecturer_course_view, name='/course/'),
     url(r'^course/(?P<fagkode>[\w]{7})/$', course.views.delegate_course_view), # TODO VI KAN KUN HA 7-TEGN FAGKODER!
-    url(r'^course/$', course.views.delegate_course_view, name='/course/'),
-    url(r'^overview/$', overview.views.courses)
->>>>>>> refs/remotes/origin/master
+    url(r'^lecturer_course/$', course.views.lecturer_course),
+    url(r'^exercise/$', exercise.views.do_exercise),
+    url(r'^exercise/(?P<exer_id>[0-9]+)/$', exercise.views.do_exercise),
     ]
