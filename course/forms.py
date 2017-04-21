ca1cc48e9ca3ab32a829b2a6247fc1537eb7dbf9
lastCent <kongekobra@msn.com>
2017-04-21 23:53:16 +0200

    PEP8
    More PEP8 stuff

--- course/forms.py
+++ course/forms.py
@@ -10,7 +10,7 @@ class CourseForm(forms.ModelForm):
 
 class PartialExerciseForm(forms.ModelForm):
     def __init__(self, course_pk, *args, **kwargs):
-        super (PartialExerciseForm,self ).__init__(*args, **kwargs) # populates the post
+        super(PartialExerciseForm, self).__init__(*args, **kwargs)  # populates the post
         self.fields['contains'].queryset = Question.objects.filter(belongsTo__pk=course_pk)
 
     class Meta:
@@ -21,9 +21,9 @@ class PartialExerciseForm(forms.ModelForm):
 
 class PartialQuestionForm(forms.ModelForm):
     def __init__(self, course_pk, *args, **kwargs):
-        super (PartialQuestionForm,self ).__init__(*args, **kwargs) # populates the post
+        super(PartialQuestionForm, self).__init__(*args, **kwargs)  # populates the post
         # Lordhavemercy, sqlite limitation workaround
-        reading_mats = list(Course.objects.get(pk=course_pk).content.values_list('pk',flat=True))
+        reading_mats = list(Course.objects.get(pk=course_pk).content.values_list('pk', flat=True))
         tags = ThemeTag.objects.filter(material__pk__in=reading_mats).values_list('pk', flat=True).distinct()
         self.fields['themeTags'].queryset = ThemeTag.objects.filter(pk__in=tags)
 
@@ -35,7 +35,7 @@ class PartialQuestionForm(forms.ModelForm):
 
 class ReadingMatForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super (ReadingMatForm,self ).__init__(*args, **kwargs) # populates the post
+        super(ReadingMatForm, self).__init__(*args, **kwargs)  # populates the post
         self.fields['link'].required = False
 
     class Meta:
@@ -43,10 +43,9 @@ class ReadingMatForm(forms.ModelForm):
         exclude = []
 
 
-
 class ThemeTagForm(forms.ModelForm):
     def __init__(self, course_pk, *args, **kwargs):
-        super (ThemeTagForm,self ).__init__(*args, **kwargs) # populates the post
+        super(ThemeTagForm, self).__init__(*args, **kwargs)  # populates the post
         self.fields['material'].queryset = Course.objects.get(pk=course_pk).content
 
     class Meta:
